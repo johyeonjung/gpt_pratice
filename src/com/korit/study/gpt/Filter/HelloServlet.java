e789acab7833bb83518dc230bee80526ea17c538
unknown <askmoon18@naver.com>
2025-11-05 07:53:03 +0900

    search메서드 구현하기

---
+++ src/com/korit/study/gpt/Filter/HelloServlet.java
@@ -0,0 +1,15 @@
+package com.korit.study.gpt.Filter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello/servlet")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
---
+++ src/com/korit/study/gpt/Filter/LoggingFilter.java
@@ -0,0 +1,14 @@
+package com.korit.study.gpt.Filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+//파일 옮김.
+
+public class LoggingFilter {
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        System.out.println("before");
+//        filterChain.doFilter(servletRequest,servletResponse);
+//        System.out.println("after");
+    }
